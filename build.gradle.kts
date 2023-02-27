fad229e4071d1f9b6f5a1c47c473cecc69466ed1
Bruno Henrique Paiva <bruno@sigane.com.br>
2023-02-27 12:05:06 -0300

    build: update AGP and use plugin aliases

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.0.0-beta03" apply false
-    id("com.android.library") version "8.0.0-beta03" apply false
-    kotlin("jvm") version "1.8.10" apply false
-    kotlin("android") version "1.8.10" apply false
-    kotlin("kapt") version "1.8.10" apply false
-    id("com.google.dagger.hilt.android") version "2.45" apply false
-    id("com.google.protobuf") version "0.9.1" apply false
-    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.kapt) apply false
+    alias(libs.plugins.hilt.android) apply false
+    alias(libs.plugins.protobuf) apply false
+    alias(libs.plugins.ksp) apply false
 }
