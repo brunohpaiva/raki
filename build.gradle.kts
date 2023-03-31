929872c50c9bfec9ed10ee0346d32e9b6114f47b
Bruno Henrique Paiva <bruno@sigane.com.br>
2023-03-31 19:18:06 -0300

    build: add gradle versions plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,4 +8,5 @@ plugins {
     alias(libs.plugins.hilt.android) apply false
     alias(libs.plugins.protobuf) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.versions)
 }
